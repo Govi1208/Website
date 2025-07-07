1d869c36402004c7f2e605bbc7fb5ac6605825a3
Govind <goviraj1208@gmail.com>
2025-07-07 15:48:31 +0530

    Upload full version of site

---
+++ Website/assets/js/script.js
@@ -0,0 +1,21 @@
+// const text = "TRADITION • NUTRITION • TRANSFORMATION • ";
+// const circularTextEl = document.getElementById("circularText");
+
+// function renderCircularText() {
+//   circularTextEl.innerHTML = "";
+//   const container = circularTextEl.getBoundingClientRect();
+//   const radius = container.width / 2.2;
+//   const textLength = text.length;
+
+//   for (let i = 0; i < textLength; i++) {
+//     const char = text[i];
+//     const span = document.createElement("span");
+//     const angle = (360 / textLength) * i;
+//     span.innerText = char;
+//     span.style.transform = `rotate(${angle}deg) translate(${radius}px) rotate(90deg)`;
+//     circularTextEl.appendChild(span);
+//   }
+// }
+
+// window.addEventListener("load", renderCircularText);
+// window.addEventListener("resize", renderCircularText);
